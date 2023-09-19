e5c1b371455d1bc7c196198cd5dfd7dc1d6af3fc
Chin Ling Ng <nchinling@Chins-MacBook-Pro.local>
2023-09-19 18:52:14 +0800

    mathquill

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 import sympy as sp
+import re
+
 
 app = Flask(__name__)
 
@@ -8,11 +10,13 @@ x = sp.symbols('x')
 
 def calculate_derivative(expression):
     derivative = sp.diff(expression, x)
+    print("the derivative is ", derivative)
     return derivative
 
 
 def calculate_integral(expression):
     integral = sp.integrate(expression, x)
+    print("the integral is ", integral)
     return integral
 
 
@@ -33,20 +37,30 @@ def calculus():
     error = None
 
     if request.method == 'POST':
-        expression_str = request.form['expression']
+
+        expression_str = request.form['mathquill_plain']
+        data_type = type(expression_str)
+        print("Data type of expression_str:", data_type)
+
+        print("the string field is ", expression_str)
         operation = request.form['operation']
 
+        cleaned_str = clean_and_sympify(expression_str)
+
         try:
-            expression = sp.sympify(expression_str)
+            print("the expression before is ", cleaned_str)
+            # expression = sp.sympify(cleaned_string)
+            # print("the expression after is ", expression)
 
             if operation == 'derivative':
-                result = calculate_derivative(expression)
+                result = calculate_derivative(cleaned_str)
                 latex_result = sp.latex(result)
                 print("Derivative Result:", latex_result)
             elif operation == 'integral':
-                result = calculate_integral(expression)
+                result = calculate_integral(cleaned_str)
                 print("Integral Result:", latex_result)
                 latex_result = sp.latex(result)
+
         except (sp.SympifyError, ValueError):
             error = "Invalid input. Please enter a valid mathematical expression."
 
@@ -60,7 +74,7 @@ def equation_solver():
     error = None
 
     if request.method == 'POST':
-        equation_str = request.form['equation']
+        equation_str = request.form['mathquill_plain']
         # Find the position of the equal sign
         equal_sign_index = equation_str.find('=')
 
@@ -70,7 +84,8 @@ def equation_solver():
             equation_str = equation_str[:equal_sign_index]
 
         try:
-            equation = sp.sympify(equation_str)
+            cleaned_str = clean_and_sympify(equation_str)
+            equation = sp.sympify(cleaned_str)
             solutions = solve_equation(equation)
             latex_solutions = sp.latex(solutions)
             print("The solution is ", latex_solutions)
@@ -78,6 +93,28 @@ def equation_solver():
             solutions = "Invalid input. Please enter a valid equation."
 
     return render_template('equation_solver.html', error=error, latex_solutions=latex_solutions)
+
+
+def clean_and_sympify(expression_str):
+  # Replace asterisks (*) with the multiplication symbol (·)
+
+    replacements = {
+        r'\s*i*n *(': 'sin(',
+        r'\c*o*s *(': 'cos(',
+        r'\t*a*n *(': 'tan(',
+        r'\l*o*g *(': 'log(',
+    }
+
+    # Iterate through the dictionary and apply replacements
+    for pattern, replacement in replacements.items():
+        expression_str = expression_str.replace(pattern, replacement)
+
+    print("the cleaned string is ", expression_str)
+    try:
+        expression = sp.sympify(expression_str)
+        return expression
+    except Exception as e:
+        return None
 
 
 if __name__ == "__main__":
